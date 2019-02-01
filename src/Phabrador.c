e651f1a7496720836316dc0470f83062a8905927
Jared Forsyth <jared@jaredforsyth.com>
2019-02-01 08:44:51 -0800

    start to fetch a bit

--- src/Phabrador.c
+++ src/Phabrador.c
@@ -1,85 +1,12 @@
 #include <fluid_shared.h>
 #include <CoreImage/CoreImage.h>
 
-CAMLprim value qmoji_grayscaleEmoji(value text_v) {
-  CAMLparam1(text_v);
-  CAMLlocal1(image_v);
-  NSString* text = NSString_val(text_v);
-
-  // Draw the emoji
-  NSDictionary* attributes = @{NSFontAttributeName: [NSFont systemFontOfSize:14.0]};
-  NSImage *image = [[NSImage alloc] initWithSize:[text sizeWithAttributes:attributes]];
-  [image lockFocus];
-  // draw the text
-  [text drawAtPoint:NSMakePoint(0, 0) withAttributes:attributes];
-  [image unlockFocus];
-
-  // Turn grayscale
-  CIFilter *filterImage = [CIFilter filterWithName: @"CIPhotoEffectMono"];
-  [filterImage setValue:[[CIImage alloc] initWithData:[image TIFFRepresentation]]
-                 forKey:kCIInputImageKey];
-  CIImage* outputImage;
-
-  BOOL isDarkMode = [[[NSUserDefaults standardUserDefaults] stringForKey:@"AppleInterfaceStyle"]
-                     isEqualToString:@"Dark"];
-  if (isDarkMode) {
-    CIFilter *filterImage2 = [CIFilter filterWithName: @"CIColorInvert"];
-    [filterImage2 setValue:filterImage.outputImage forKey:kCIInputImageKey];
-    outputImage = filterImage2.outputImage;
-  } else {
-    outputImage = filterImage.outputImage;
-  }
-
-  CGRect rect = CGRectMake(0, 0, image.size.width, image.size.height);
-  NSCIImageRep *rep = [NSCIImageRep imageRepWithCIImage:outputImage];
-  NSImage* newImage = [[NSImage alloc] initWithSize:rect.size];
-  [newImage addRepresentation:rep];
-
-  Wrap(image_v, newImage);
-  CAMLreturn(image_v); 
-}
-
-NSData* qmoji_dataForText(NSString* text) {
-  NSFont* nsFont = [NSFont systemFontOfSize:8.0];
-  NSDictionary* attributes = @{NSFontAttributeName: nsFont};
-  NSImage *img = [[NSImage alloc] initWithSize:[text sizeWithAttributes:attributes]];
-  [img lockFocus];
-  [text drawAtPoint:NSMakePoint(10, 10) withAttributes:attributes];
-  [img unlockFocus];
-  return [img TIFFRepresentation];
-}
-
-BOOL _qmoji_isEmojiSupported(NSString* text) {
-  static NSData * UNAVAILABLE = NULL;
-  if (UNAVAILABLE == NULL) {
-    UNAVAILABLE = qmoji_dataForText(@"\u1fff");
-  }
-  return ![qmoji_dataForText(text) isEqualToData:UNAVAILABLE];
-}
-
-CAMLprim value qmoji_current_mouse(value window_v) {
-  CAMLparam1(window_v);
-  CAMLlocal1(pos_v);
-
-  NSPoint loc = [(id)Unwrap(window_v) mouseLocationOutsideOfEventStream];
-  NSPoint window_origin = ((NSWindow*)Unwrap(window_v)).frame.origin;
-
-  Create_record2_double(pos_v, loc.x + window_origin.x, loc.y + window_origin.y);
-
-  CAMLreturn(pos_v);
-}
-
-CAMLprim value qmoji_isEmojiSupported(value text) {
-  CAMLparam1(text);
-  CAMLreturn(_qmoji_isEmojiSupported(NSString_val(text)) ? Val_true : Val_false);
-}
-
-CAMLprim value qmoji_homeDirectory() {
+CAMLprim value phabrador_homeDirectory() {
   CAMLparam0();
   CAMLreturn(caml_copy_string([NSHomeDirectory() UTF8String]));
 }
 
-void qmoji_setTimeout(value callback, value milis_v) {
+void phabrador_setTimeout(value callback, value milis_v) {
   CAMLparam2(callback, milis_v);
   int64_t milis = Int_val(milis_v);
   int cbid = Int_val(callback);
@@ -91,7 +18,7 @@ void qmoji_setTimeout(value callback, value milis_v) {
   CAMLreturn0;
 }
 
-void qmoji_openUrl(value url) {
+void phabrador_openUrl(value url) {
   CAMLparam1(url);
   [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString: NSString_val(url)]];
   CAMLreturn0;
@@ -105,22 +32,46 @@ void qmenu_toggleMenuItem(value item_v, value isOn) {
   CAMLreturn0;
 }
 
-void qmoji_showMenu(value menu_v) {
+void phabrador_showMenu(value menu_v) {
   CAMLparam1(menu_v);
   NSMenu* menu = (NSMenu*)Unwrap(menu_v);
   [NSMenu popUpContextMenu:menu withEvent:[NSApp currentEvent] forView:nil];
   CAMLreturn0;
 }
 
-void qmoji_fetch(value url, value callback) {
+void phabrador_fetch(value url, value callback, value headers) {
   CAMLparam1(url);
   int callbackId = Int_val(callback);
+  NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:[[NSURL alloc] initWithString:NSString_val(url)]];
+  int length = Wosize_val(headers);
+  for (int i=0; i<length; i++) {
+    value name_v = Field(Field(headers, i), 0);
+    NSString* name = NSString_val(name_v);
+    value value_v = Field(Field(headers, i), 1);
+    NSString* value = NSString_val(value_v);
+    [request setValue:value forHTTPHeaderField:name];
+  }
   NSURLSessionDataTask* task = [[NSURLSession sharedSession]
-      dataTaskWithURL:[[NSURL alloc] initWithString:NSString_val(url)]
+      dataTaskWithRequest:request
     completionHandler:^(NSData* data, NSURLResponse *response, NSError *error){
+      NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse*)response;
       NSString *text = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
       dispatch_async(dispatch_get_main_queue(), ^{
-        callString(callbackId, [text UTF8String]);
+        CAMLparam0();
+        CAMLlocal1(tuple_v);
+
+        static value * closure_f = NULL;
+        if (closure_f == NULL) {
+            /* First time around, look up by name */
+            closure_f = caml_named_value("phabrador_fetch_cb");
+        }
+
+        tuple_v = caml_alloc_tuple(2);
+        Store_field(tuple_v, 0, caml_copy_string([text UTF8String]));
+        Store_field(tuple_v, 1, Val_int(httpResponse.statusCode));
+
+        caml_callback2(*closure_f, Val_int(callbackId), tuple_v);
+        CAMLreturn0;
       });
     }];
   [task resume];
--- src/Phabrador.re
+++ src/Phabrador.re
@@ -4,7 +4,36 @@ open FluidMac;
 
 let str = Fluid.string;
 
-let main = (~assetsDir as _, _hooks) => {
+module FetchTracker = Tracker({type arg = (string, int); let name = "phabrador_fetch_cb"; let once = true;});
+
+external homeDirectory: unit => string = "phabrador_homeDirectory";
+external fetch: (~url: string, ~callback: FetchTracker.callbackId, ~headers: array((string, string))) => unit = "phabrador_fetch";
+let fetch = (~url, ~headers=[||], callback) => fetch(~url, ~callback=FetchTracker.track(callback), ~headers);
+
+let getAuth = () => {
+  let fpath = Filename.concat(homeDirectory(), ".arcrc");
+  let json = Json.parse(Files.readFileExn(fpath));
+  open Json.Infix;
+  let hosts = json |> Json.get("hosts") |?> Json.obj |? [];
+  switch hosts {
+    | [] => failwith("No hosts in .arcrc")
+    | [(hostname, obj), ..._] => switch (obj |> Json.get("token") |?> Json.string) {
+      | None => failwith("No token for host in .arcrc")
+      | Some(token) => (hostname, token)
+    }
+  }
+};
+
+let (hostname, token) = getAuth();
+
+let main = (~assetsDir as _, hooks) => {
+  let%hook () = Fluid.Hooks.useEffect(() => {
+    fetch(~url=hostname ++ "user.whoami?api.token=" ++ token, ((body, status)) => {
+      print_endline("Success! " ++ string_of_int(status));
+      print_endline(body)
+    });
+    () => ()
+  }, ());
   <view>
     {str("Hello")}
   </view>
